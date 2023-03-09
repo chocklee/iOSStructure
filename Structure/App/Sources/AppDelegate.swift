36aaae3ebcf6df23a12ff2f64e0b60bd5740f3b6
Changhao Li <chanli@linkedin.com>
2023-03-09 18:17:28 +0800

    删除main.storyboard

--- Structure/App/Sources/AppDelegate.swift
+++ Structure/App/Sources/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         AppRootContainer.setupForApp()
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.backgroundColor = .white
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 }
